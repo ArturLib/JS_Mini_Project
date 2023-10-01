64ab2a37df1c7562cb5ac8700ba1476ea113567f
ArturLib <arturliber@gmail.com>
2023-10-01 21:49:24 +0300

    added styles to the project

--- user-details/user-details.js
+++ user-details/user-details.js
@@ -3,61 +3,107 @@ let userId = new URL(location.href).searchParams.get('userId');
 let gettingUserInfo = async () => {
     let response = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
     let user = await response.json();
-    let userInfoDiv = document.createElement('div');
 
-    userInfoDiv.innerText =
-        `
-        ID - ${user.id}
-        NAME - ${user.name}
-        USER NAME - ${user.username}
-        EMAIL - ${user.email}
-        PHONE - ${user.phone}
-        WEBSITE - ${user.website}
-        ADDRESS:
-        city - ${user.address.city}
-        street - ${user.address.street}
-        suite - ${user.address.suite}
-        zipcode - ${user.address.zipcode}
-        GEO:
-        latitude - ${user.address.geo.lat}
-        longitude - ${user.address.geo.lng}
-        COMPANY:
-        ${user.company.bs} 
-        ${user.company.catchPhrase}
-        ${user.company.name}
-        `
+    let header = document.createElement('div');
+    header.innerText = 'Information about the USER';
+    header.classList.add('header');
 
     let wrapper = document.createElement('div');
-    wrapper.append(userInfoDiv);
+    wrapper.classList.add('wrapper');
 
-    let postOfUser = document.createElement('button');
-    postOfUser.innerText = 'post of current user';
-    postOfUser.onclick = () => {
+    let userInfoDiv = document.createElement('div');
+    userInfoDiv.classList.add('user-info');
+
+    let idDiv = document.createElement('div');
+    idDiv.innerText = `${user.id}`;
+    idDiv.classList.add('id-style');
+
+    let nameDiv = document.createElement('div');
+    nameDiv.innerText = `${user.name}`;
+    nameDiv.classList.add('name-style');
+
+    let userNameDiv = document.createElement('div');
+    userNameDiv.innerText = `${user.username}`;
+    userNameDiv.classList.add('userName-style');
+
+    let emailDiv = document.createElement('div');
+    emailDiv.innerText = `${user.email}`;
+    emailDiv.classList.add('email-style');
+
+    userInfoDiv.append(idDiv, nameDiv, userNameDiv, emailDiv);
+
+    let infoDiv = document.createElement('div');
+    infoDiv.classList.add('info-style');
+
+    let addressDiv = document.createElement('div');
+    addressDiv.innerText =
+        `
+        ADDRESS:
+        Street: ${user.address.street}
+        Suite: ${user.address.suite}
+        City: ${user.address.city}
+        Zipcode: ${user.address.zipcode}
+        `;
+    addressDiv.classList.add('address-style');
+
+    let geoDiv = document.createElement('div');
+    geoDiv.innerText =
+        `
+        GEO:
+        lat: ${user.address.geo.lat}
+        lng: ${user.address.geo.lng}
+        `;
+    geoDiv.classList.add('geo-style');
+
+    let contactsDiv = document.createElement('div');
+    contactsDiv.innerText =
+        `
+        CONTACTS:
+        Phone: ${user.phone}
+        Website: ${user.website}
+        Company: ${user.company.name}
+        ${user.company.bs} 
+        ${user.company.catchPhrase}
+        `;
+    contactsDiv.classList.add('contacts-div');
+
+    infoDiv.append(addressDiv, geoDiv, contactsDiv);
+
+    let button = document.createElement('button');
+    button.innerText = 'posts of current user';
+    button.onclick = () => {
         gettingPosts();
     }
+    button.classList.add('button-current-post')
 
-    document.body.append(wrapper, postOfUser);
+    wrapper.append(userInfoDiv, infoDiv, button);
+
+    document.body.append(header, wrapper);
 }
 void gettingUserInfo();
 
 let gettingPosts = async () => {
     let response2 = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`);
     let posts = await response2.json();
+
     let postsDivEl = document.createElement('div');
+    postsDivEl.classList.add('post-wrapper');
 
     for (const post of posts) {
         let postDivEl = document.createElement('div');
-        postDivEl.innerText = `title - ${post.title}`;
+        postDivEl.innerText = `TITLE: ${post.title}`;
+        postDivEl.classList.add('post-title');
 
         let button = document.createElement('button');
         button.innerText = 'more info';
+        button.classList.add('post-button');
         button.onclick = () => {
             location.href = `../post-details/post-details.html?postsId=${post.id}`;
         }
 
-        postsDivEl.append(postDivEl, button);
-
+        postDivEl.appendChild(button);
+        postsDivEl.appendChild(postDivEl);
     }
 
-    document.body.appendChild(postsDivEl);
+    document.body.append(postsDivEl);
 }
